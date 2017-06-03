47faca2f93ad81451d62daa67f453c297742a2ac
Jason Lenoble <jason.lenoble@gmail.com>
2017-06-04 00:12:00 +0200

    class Path
    A wrapper around absolute paths. (Paths are always converted to absolute
    in ctor)

---
+++ src/path.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import untildify from 'untildify';
+
+class Path {
+  constructor (_path) {
+    Object.defineProperty(this, 'path', {
+      value: path.resolve(untildify(_path)),
+    });
+  }
+}
+
+const proto = Path.prototype;
+
+['dirname', 'basename', 'extname'].forEach(key => {
+  proto[key] = function () {
+    return path[key](this.path);
+  };
+});
+
+export default Path;
