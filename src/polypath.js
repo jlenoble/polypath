d1d15111852cb2f80493f5db47e57990be81adcf
Jason Lenoble <jason.lenoble@gmail.com>
2017-07-31 15:29:28 +0200

    Tested methods for PolyPath
    basenames, dirnames, extnames.

--- src/polypath.js
+++ src/polypath.js
@@ -1,5 +1,32 @@
-export default class Polypath {
-  constructor () {
-    console.log('Hello world!');
-  }
-}
+import path from 'path';
+import untildify from 'untildify';
+import Path from './path';
+import {PolytonFactory} from 'polyton';
+
+const PolyPath = PolytonFactory(Path,
+['literal'], [{unordered: true}], {
+  preprocess: function (args) {
+    return args.map(([arg]) => [path.resolve(untildify(arg))]);
+  },
+  properties: {
+    paths: {
+      get () {
+        return this.map(p => p.path);
+      },
+    },
+  },
+  extend: {
+    basenames () {
+      return this.map(p => p.basename());
+    },
+    dirnames () {
+      return this.map(p => p.dirname());
+    },
+    extnames () {
+      return this.map(p => p.extname());
+    },
+  },
+});
+
+export default PolyPath;
+export {Path};
--- test/polypath.test.js
+++ test/polypath.test.js
@@ -1,12 +1,41 @@
-import Muter, {captured} from 'muter';
 import {expect} from 'chai';
-import Polypath from '../src/polypath';
+import os from 'os';
+import path from 'path';
+import PolyPath from '../src/polypath';
 
-describe('Testing Polypath', function () {
-  const muter = Muter(console, 'log'); // eslint-disable-line new-cap
+describe('Testing PolyPath', function () {
+  it(`Testing strict equality`, function () {
+    const p1 = new PolyPath('~', '~/projects/keyfunc', '../src/muter.js');
+    const p2 = new PolyPath(
+      '~/projects/keyfunc',
+      os.homedir(),
+      path.join(process.cwd(), '../src/muter.js')
+    );
 
-  it(`Class Polypath says 'Hello world!'`, captured(muter, function () {
-    new Polypath();
-    expect(muter.getLogs()).to.equal('Hello world!\n');
-  }));
+    expect(p1).to.equal(p2);
+  });
+
+  it(`paths, basenames, dirnames and extnames properties`, function () {
+    const p = new PolyPath('~', '~/projects/keyfunc', '../src/muter.js');
+
+    expect(p.paths).to.eql([
+      os.homedir(),
+      path.join(os.homedir(), 'projects/keyfunc'),
+      path.join(process.cwd(), '../src/muter.js'),
+    ]);
+
+    expect(p.dirnames()).to.eql([
+      path.dirname(os.homedir()),
+      path.join(os.homedir(), 'projects'),
+      path.join(process.cwd(), '../src'),
+    ]);
+
+    expect(p.basenames()).to.eql([
+      path.basename(os.homedir()),
+      'keyfunc',
+      'muter.js',
+    ]);
+
+    expect(p.extnames()).to.eql(['', '', '.js']);
+  });
 });
