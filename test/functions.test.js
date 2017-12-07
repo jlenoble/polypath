5ba5debd36150faaf41a37702b2e621e17658831
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-07 15:44:49 +0100

    Renaming and removal
    resolve->resolveGlob
    rebase->rebaseGlob
    
    resolveGlob doesn't handle any more array of arrays

--- test/functions.test.js
+++ test/functions.test.js
@@ -1,50 +1,47 @@
 import {expect} from 'chai';
 import path from 'path';
-import {rebase, resolve} from '../src/polypath';
+import {rebaseGlob, resolveGlob} from '../src/polypath';
 
 describe('Testing package functions', function () {
-  it(`Testing rebase`, function () {
-    expect(rebase('src/*.js', 'build')).to.eql([path.join(process.cwd(),
+  it(`Testing rebaseGlob`, function () {
+    expect(rebaseGlob('src/*.js', 'build')).to.eql([path.join(process.cwd(),
       'build/src/*.js')]);
-    expect(rebase(['src/*.js'], 'build')).to.eql([path.join(process.cwd(),
+    expect(rebaseGlob(['src/*.js'], 'build')).to.eql([path.join(process.cwd(),
       'build/src/*.js')]);
-    expect(rebase(['src/*.js', 'test/*.js'], 'build')).to.eql([
+    expect(rebaseGlob(['src/*.js', 'test/*.js'], 'build')).to.eql([
       path.join(process.cwd(), 'build/src/*.js'),
       path.join(process.cwd(), 'build/test/*.js'),
     ]);
 
-    expect(rebase('src/*.js', 'src', 'build')).to.eql([path.join(process.cwd(),
-      'build/*.js')]);
-    expect(rebase(['src/*.js'], 'src', 'build')).to.eql(
+    expect(rebaseGlob('src/*.js', 'src', 'build')).to.eql([
+      path.join(process.cwd(), 'build/*.js')]);
+    expect(rebaseGlob(['src/*.js'], 'src', 'build')).to.eql(
       [path.join(process.cwd(), 'build/*.js')]);
-    expect(rebase(['src/*.js', 'test/*.js'], 'src', 'build')).to.eql([
+    expect(rebaseGlob(['src/*.js', 'test/*.js'], 'src', 'build')).to.eql([
       path.join(process.cwd(), 'build/*.js'),
       path.join(process.cwd(), 'test/*.js'), // cwd/src/../test -> cwd/test
     ]);
   });
 
-  it(`Testing resolve`, function () {
+  it(`Testing resolveGlob`, function () {
     const paths = ['.babelrc', 'src/path.js', 'src/polypath.js'].map(
       f => path.join(process.cwd(), f));
 
     return Promise.all([
-      resolve('src/*.js'),
-      resolve(['src/*.js']),
-      resolve('.babelrc'),
-      resolve(['src/*.js', '.babelrc']),
-      resolve('src/*.js', '.babelrc'),
-      resolve(['src/*.js'], ['.babelrc']),
-      resolve(['src/*.js'], '.babelrc'),
-      resolve('src/*.js', ['.babelrc']),
-      resolve([['src/*.js'], '.babelrc']),
-      resolve(['src/*.js', ['.babelrc']]),
-      resolve([['src/*.js'], ['.babelrc']]),
+      resolveGlob('src/*.js'),
+      resolveGlob(['src/*.js']),
+      resolveGlob('.babelrc'),
+      resolveGlob(['src/*.js', '.babelrc']),
+      resolveGlob('src/*.js', '.babelrc'),
+      resolveGlob(['src/*.js'], ['.babelrc']),
+      resolveGlob(['src/*.js'], '.babelrc'),
+      resolveGlob('src/*.js', ['.babelrc']),
     ]).then(files => {
       expect(files).to.eql([
         ['path.js', 'polypath.js'].map(f => path.join(process.cwd(), 'src', f)),
         ['path.js', 'polypath.js'].map(f => path.join(process.cwd(), 'src', f)),
         ['.babelrc'].map(f => path.join(process.cwd(), f)),
-        paths, paths, paths, paths, paths, paths, paths, paths,
+        paths, paths, paths, paths, paths,
       ]);
     });
   });
