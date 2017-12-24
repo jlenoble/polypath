f99fcf494352fae30c8db1d9718feee11be01252
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-24 13:42:52 +0100

    Add dynamically 'add' methods
    Function add encapsulates the cross-referencing logic.

---
+++ src/add.js
@@ -0,0 +1,98 @@
+import {error} from 'explanation';
+
+export default function add (Type1, Type2, implementation, options) {
+  if (!(Type1 instanceof Function)) {
+    error({
+      message: 'Not a prototype',
+      explain: [
+        ['In function:', add.name],
+        'You tried to access the prototype of Type1',
+        'But Type1 is not a function',
+        ['It is:', Type1],
+      ],
+    });
+  }
+
+  if (!(Type2 instanceof Function)) {
+    error({
+      message: 'Not a prototype',
+      explain: [
+        ['In function:', add.name],
+        'You tried to access the prototype of Type2',
+        'But Type2 is not a function',
+        ['It is:', Type2],
+      ],
+    });
+  }
+
+  const p2 = Type2.prototype;
+
+  const symName1 = `type:${Type1.name}`;
+  const symName2 = `add:${Type2.name}`;
+
+  const symbs1 = Object.getOwnPropertySymbols(p2);
+  const symb1 = Symbol(symName1);
+  const index1 = symbs1.map(s => s.toString()).indexOf(symb1.toString());
+
+  if (index1 !== -1) {
+    if (p2[symbs1[index1]] !== Type1) {
+      error({
+        message: `Same 'type' symbol, but different type`,
+        explain: [
+          ['You attempted to set symbol', symb1.toString()],
+          ['on the prototype of type', Type2.name],
+          ['with the intention to target', Type1.name],
+          'But the symbol already exists',
+          'and points to a parasitic type with same name',
+        ],
+      });
+    }
+  }
+
+  const _type1 = symb1;
+  p2[_type1] = Type1;
+
+  const symbs2 = Object.getOwnPropertySymbols(Type1);
+  const symb2 = Symbol(symName2);
+  const index2 = symbs2.map(s => s.toString()).indexOf(symb2.toString());
+
+  if (index2 !== -1) {
+    error({
+      message: `${Type1.name}::add(${Type2.name}) already implemented`,
+      explain: [
+        ['You attempted to set symbol', symb2.toString()],
+        ['on type', Type1.name],
+        'But the symbol already exists',
+        `This means that the 'add' method has already been implemented`,
+        ['for intended type', Type2.name],
+        'or another parasitic type sharing its name',
+      ],
+    });
+  }
+
+  const _add2 = index2 === -1 ? symb2 : symbs2[index2];
+  Type1[_add2] = implementation; // eslint-disable-line no-param-reassign
+
+  p2.add = function (a) {
+    try {
+      return a[_type1][_add2].call(this, a);
+    } catch (e) {
+      if (new RegExp(`Cannot read property.*add:${Type2.name}.*of undefined`)
+        .test(e.message)) {
+        const _p2 = Object.getPrototypeOf(a).name || typeof a;
+
+        error({
+          message: `${Type1.name} cannot add with ${_p2}`,
+          explain: [
+            ['You tried to add:', this],
+            ['of type', Type1.name],
+            ['with:', a],
+            ['of type', _p2],
+          ],
+        });
+      }
+
+      throw e;
+    }
+  };
+}
---
+++ test/add.test.js
@@ -0,0 +1,181 @@
+/* eslint-disable no-invalid-this */
+import {expect} from 'chai';
+
+import add from '../src/add';
+
+import Chunk from '../src/chunk';
+import Chunks from '../src/chunks';
+
+import Muter, {muted} from 'muter';
+// import Muter, {captured as muted} from 'muter';
+
+const muter = Muter(console);
+
+describe(`'add' function`, function () {
+  describe(`provides a type with an 'add' property that`, function () {
+    beforeEach(function () {
+      class Type1 extends Chunk {};
+      class Type3 extends Chunks {};
+
+      this.Type1 = Type1;
+      this.Type3 = Type3;
+
+      add(Type1, Type1, function (obj) {
+        return new Type3(this.chunk + ',' + obj.chunk);
+      });
+    });
+
+    it('is a function', function () {
+      const {Type1} = this;
+
+      const t1 = new Type1('a');
+      const t2 = new Type1('b');
+
+      expect(t1.add).to.be.instanceof(Function);
+      expect(() => t1.add(t2)).not.to.throw();
+    });
+
+    it('returns the expected type', function () {
+      const {Type1, Type3} = this;
+
+      const t1 = new Type1('a');
+      const t2 = new Type1('b');
+
+      expect(t1.add(t2)).to.be.instanceof(Type3);
+    });
+
+    it('throws on unexpected type', muted(muter, function () {
+      const {Type1} = this;
+
+      const t1 = new Type1('a');
+      const t2 = 'b';
+
+      expect(() => t1.add(t2)).to.throw('Type1 cannot add with string');
+    }));
+  });
+
+  describe(`when run twice`, function () {
+    beforeEach(function () {
+      class Type1 extends Chunk {};
+      class Type2 extends Chunk {};
+      class Type3 extends Chunks {};
+
+      this.Type1 = Type1;
+      this.Type2 = Type2;
+      this.Type3 = Type3;
+
+      add(Type1, Type2, function (obj) {
+        return new Type3(this.chunk + ',' + obj.chunk);
+      });
+
+      const p2 = Type2.prototype;
+      const p3 = Type3.prototype;
+
+      const storeT1OnP2 = `type:${Type1.name}`;
+      const storeT1OnP3 = storeT1OnP2;
+      const storeT3OnP2 = `type:${Type3.name}`;
+      const addT2ToT1 = `add:${Type2.name}`;
+      const addT3ToT1 = `add:${Type3.name}`;
+      const addT2ToT3 = addT2ToT1;
+
+      const index = (sym1, symName2) => () => {
+        const symbs = Object.getOwnPropertySymbols(sym1);
+        return symbs.map(s => s.toString()).indexOf(
+          Symbol(symName2).toString());
+      };
+
+      this.indexStoreT1OnP2 = index(p2, storeT1OnP2);
+      this.indexStoreT1OnP3 = index(p3, storeT1OnP3);
+      this.indexStoreT3OnP2 = index(p2, storeT3OnP2);
+      this.indexAddT2ToT1 = index(Type1, addT2ToT1);
+      this.indexAddT3ToT1 = index(Type1, addT3ToT1);
+      this.indexAddT2ToT3 = index(Type3, addT2ToT3);
+
+      const length = obj => () => {
+        return Object.getOwnPropertySymbols(obj).length;
+      };
+
+      this.lengthStoreOnP2 = length(p2);
+      this.lengthStoreOnP3 = length(p3);
+      this.lengthStoreOnT1 = length(Type1);
+      this.lengthStoreOnT3 = length(Type3);
+    });
+
+    it('is ok if 2nd type is different from before', function () {
+      expect(this.indexStoreT1OnP2()).to.equal(0);
+      expect(this.indexStoreT1OnP3()).to.equal(-1);
+      expect(this.indexStoreT3OnP2()).to.equal(-1);
+      expect(this.indexAddT2ToT1()).to.equal(0);
+      expect(this.indexAddT3ToT1()).to.equal(-1);
+      expect(this.indexAddT2ToT3()).to.equal(-1);
+
+      expect(this.lengthStoreOnP2()).to.equal(1);
+      expect(this.lengthStoreOnP3()).to.equal(0);
+      expect(this.lengthStoreOnT1()).to.equal(1);
+      expect(this.lengthStoreOnT3()).to.equal(0);
+
+      add(this.Type1, this.Type3, function () {});
+
+      expect(this.indexStoreT1OnP2()).to.equal(0);
+      expect(this.indexStoreT1OnP3()).to.equal(0);
+      expect(this.indexStoreT3OnP2()).to.equal(-1);
+      expect(this.indexAddT2ToT1()).to.equal(0);
+      expect(this.indexAddT3ToT1()).to.equal(1);
+      expect(this.indexAddT2ToT3()).to.equal(-1);
+
+      expect(this.lengthStoreOnP2()).to.equal(1);
+      expect(this.lengthStoreOnP3()).to.equal(1);
+      expect(this.lengthStoreOnT1()).to.equal(2);
+      expect(this.lengthStoreOnT3()).to.equal(0);
+    });
+
+    it('is ok if 1st type is different from before', function () {
+      expect(this.indexStoreT1OnP2()).to.equal(0);
+      expect(this.indexStoreT1OnP3()).to.equal(-1);
+      expect(this.indexStoreT3OnP2()).to.equal(-1);
+      expect(this.indexAddT2ToT1()).to.equal(0);
+      expect(this.indexAddT3ToT1()).to.equal(-1);
+      expect(this.indexAddT2ToT3()).to.equal(-1);
+
+      expect(this.lengthStoreOnP2()).to.equal(1);
+      expect(this.lengthStoreOnP3()).to.equal(0);
+      expect(this.lengthStoreOnT1()).to.equal(1);
+      expect(this.lengthStoreOnT3()).to.equal(0);
+
+      add(this.Type3, this.Type2, function () {});
+
+      expect(this.indexStoreT1OnP2()).to.equal(0);
+      expect(this.indexStoreT1OnP3()).to.equal(-1);
+      expect(this.indexStoreT3OnP2()).to.equal(1);
+      expect(this.indexAddT2ToT1()).to.equal(0);
+      expect(this.indexAddT3ToT1()).to.equal(-1);
+      expect(this.indexAddT2ToT3()).to.equal(0);
+
+      expect(this.lengthStoreOnP2()).to.equal(2);
+      expect(this.lengthStoreOnP3()).to.equal(0);
+      expect(this.lengthStoreOnT1()).to.equal(1);
+      expect(this.lengthStoreOnT3()).to.equal(1);
+    });
+
+    it('throws if both types are the same as before', muted(muter, function () {
+      expect(() => add(this.Type1, this.Type2, function () {}))
+        .to.throw('Type1::add(Type2) already implemented');
+    }));
+
+    it('throws if 1st type is different but with same name as before',
+      muted(muter, function () {
+        function Type1 () {};
+
+        expect(() => add(Type1, this.Type2, function () {}))
+          .to.throw(`Same 'type' symbol, but different type`);
+      }));
+
+    it('throws if 2nd type is different but with same name as before',
+      muted(muter, function () {
+        function Type2 () {};
+
+        expect(() => add(this.Type1, Type2, function () {}))
+          .to.throw('Type1::add(Type2) already implemented');
+      }));
+  });
+});
