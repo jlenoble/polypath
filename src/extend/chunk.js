1d01a3cb49f8637c98f65dabdd7c4cf5823c61ec
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-28 12:41:17 +0100

    Apis Chunk/Star Chunk/Empty

--- src/extend/chunk.js
+++ src/extend/chunk.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-invalid-this */
 import {add, remove, equals, isDistinct, includes, isIncluded, includesStrictly,
   isIncludedStrictly, overlaps, overlapsStrictly} from '../methods';
-import Chunk from '../chunk';
-import Chunks from '../chunks';
+import Chunk, {StarChunk, Star, Empty} from '../chunk';
+import Chunks, {StarChunks, MixedChunks} from '../chunks';
+
+const empty = new Empty();
 
 add(Chunk, Chunk, function (obj) {
   return this.chunk === obj.chunk ? obj : new Chunks(
@@ -34,4 +36,66 @@ overlaps(Chunk, Chunk, function (obj) {
 });
 overlapsStrictly(Chunk, Chunk, function (obj) {
   return false;
+});
+
+add(Chunk, Star, function (obj) {
+  return obj;
+});
+remove(Chunk, Star, function (obj) {
+  return empty;
+});
+equals(Chunk, Star, function (obj) {
+  return false;
+});
+isDistinct(Chunk, Star, function (obj) {
+  return false;
+});
+includes(Chunk, Star, function (obj) {
+  return false;
+});
+isIncluded(Chunk, Star, function (obj) {
+  return true;
+});
+includesStrictly(Chunk, Star, function (obj) {
+  return false;
+});
+isIncludedStrictly(Chunk, Star, function (obj) {
+  return true;
+});
+overlaps(Chunk, Star, function (obj) {
+  return true;
+});
+overlapsStrictly(Chunk, Star, function (obj) {
+  return true;
+});
+
+add(Chunk, Empty, function (obj) {
+  return this;
+});
+remove(Chunk, Empty, function (obj) {
+  return this;
+});
+equals(Chunk, Empty, function (obj) {
+  return false;
+});
+isDistinct(Chunk, Empty, function (obj) {
+  return true;
+});
+includes(Chunk, Empty, function (obj) {
+  return true;
+});
+isIncluded(Chunk, Empty, function (obj) {
+  return false;
+});
+includesStrictly(Chunk, Empty, function (obj) {
+  return true;
+});
+isIncludedStrictly(Chunk, Empty, function (obj) {
+  return false;
+});
+overlaps(Chunk, Empty, function (obj) {
+  return true;
+});
+overlapsStrictly(Chunk, Empty, function (obj) {
+  return true;
 });
