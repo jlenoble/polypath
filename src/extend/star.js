a39d620a1815fa42051bc9705ef74e73a4489a63
Jason Lenoble <jason.lenoble@gmail.com>
2017-12-30 21:22:29 +0100

    Simplified Star APIs
    They are most alike some so declared them in one sweep
    using forEach.

--- src/extend/star.js
+++ src/extend/star.js
@@ -10,36 +10,6 @@ import Chunks, {StarChunks, MixedChunks} from '../chunks';
 
 
 // ***************************************************************************
-// Star/Chunk API
-// ***************************************************************************
-add(Star, Chunk, _this);
-remove(Star, Chunk, _toBeImplemented);
-equals(Star, Chunk, _false);
-isDistinct(Star, Chunk, _false);
-includes(Star, Chunk, _true);
-includesStrictly(Star, Chunk, _true);
-isIncluded(Star, Chunk, _false);
-isIncludedStrictly(Star, Chunk, _false);
-overlaps(Star, Chunk, _true);
-overlapsStrictly(Star, Chunk, _false);
-
-
-// ***************************************************************************
-// Star/StarChunk API
-// ***************************************************************************
-add(Star, StarChunk, _this);
-remove(Star, StarChunk, _toBeImplemented);
-equals(Star, StarChunk, _false);
-isDistinct(Star, StarChunk, _false);
-includes(Star, StarChunk, _true);
-includesStrictly(Star, StarChunk, _true);
-isIncluded(Star, StarChunk, _false);
-isIncludedStrictly(Star, StarChunk, _false);
-overlaps(Star, StarChunk, _true);
-overlapsStrictly(Star, StarChunk, _false);
-
-
-// ***************************************************************************
 // Star/Star API
 // ***************************************************************************
 add(Star, Star, _this);
@@ -55,60 +25,48 @@ overlapsStrictly(Star, Star, _false);
 
 
 // ***************************************************************************
+// Star/any type APIs
+// ***************************************************************************
+[Chunk, StarChunk, Empty, Chunks, StarChunks, MixedChunks].forEach(
+  Type => {
+    add(Star, Type, _this);
+    equals(Star, Type, _false);
+    isDistinct(Star, Type, _false);
+    includes(Star, Type, _true);
+    includesStrictly(Star, Type, _true);
+    isIncluded(Star, Type, _false);
+    isIncludedStrictly(Star, Type, _false);
+    overlaps(Star, Type, _true);
+    overlapsStrictly(Star, Type, _false);
+  });
+
+
+// ***************************************************************************
+// Star/Chunk API
+// ***************************************************************************
+remove(Star, Chunk, _toBeImplemented);
+
+// ***************************************************************************
+// Star/StarChunk API
+// ***************************************************************************
+remove(Star, StarChunk, _toBeImplemented);
+
+// ***************************************************************************
 // Star/Empty API
 // ***************************************************************************
-add(Star, Empty, _this);
 remove(Star, Empty, _this);
-equals(Star, Empty, _false);
-isDistinct(Star, Empty, _false);
-includes(Star, Empty, _true);
-includesStrictly(Star, Empty, _true);
-isIncluded(Star, Empty, _false);
-isIncludedStrictly(Star, Empty, _false);
-overlaps(Star, Empty, _true);
-overlapsStrictly(Star, Empty, _false);
-
 
 // ***************************************************************************
 // Star/Chunks API
 // ***************************************************************************
-add(Star, Chunks, _this);
 remove(Star, Chunks, _toBeImplemented);
-equals(Star, Chunks, _false);
-isDistinct(Star, Chunks, _false);
-includes(Star, Chunks, _true);
-includesStrictly(Star, Chunks, _true);
-isIncluded(Star, Chunks, _false);
-isIncludedStrictly(Star, Chunks, _false);
-overlaps(Star, Chunks, _true);
-overlapsStrictly(Star, Chunks, _false);
-
 
 // ***************************************************************************
 // Star/StarChunks API
 // ***************************************************************************
-add(Star, StarChunks, _this);
 remove(Star, StarChunks, _toBeImplemented);
-equals(Star, StarChunks, _false);
-isDistinct(Star, StarChunks, _false);
-includes(Star, StarChunks, _true);
-includesStrictly(Star, StarChunks, _true);
-isIncluded(Star, StarChunks, _false);
-isIncludedStrictly(Star, StarChunks, _false);
-overlaps(Star, StarChunks, _true);
-overlapsStrictly(Star, StarChunks, _false);
-
 
 // ***************************************************************************
 // Star/MixedChunks API
 // ***************************************************************************
-add(Star, MixedChunks, _this);
 remove(Star, MixedChunks, _toBeImplemented);
-equals(Star, MixedChunks, _false);
-isDistinct(Star, MixedChunks, _false);
-includes(Star, MixedChunks, _true);
-includesStrictly(Star, MixedChunks, _true);
-isIncluded(Star, MixedChunks, _false);
-isIncludedStrictly(Star, MixedChunks, _false);
-overlaps(Star, MixedChunks, _true);
-overlapsStrictly(Star, MixedChunks, _false);
